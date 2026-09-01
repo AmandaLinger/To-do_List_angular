b431fc69aa8be9fedce3732f008eb0d7e44d4093
amanda <amandamarqueslinger@gmail.com>
2026-09-01 15:20:18 -0300

    Criando o método de adicionar tarefa

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {TaskCardComponent} from '../task-card/task-card.component';
+import {TaskService} from '../../services/task.service';
 
 @Component({
   selector: 'app-task-list-section',
@@ -10,5 +11,11 @@ import {TaskCardComponent} from '../task-card/task-card.component';
   styleUrl: './task-list-section.component.css'
 })
 export class TaskListSectionComponent {
+    private readonly _taskService = inject(TaskService);
 
+    ngOnInit() {
+      this._taskService.todoTasks.subscribe((todoList) => {
+        console.log('Lista de to-do: ', todoList);
+      });
+    }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 import {ITask} from '../interfaces/task.interface';
+import {ITaskFormControls} from '../interfaces/task-form-controls.interface';
+import {TaskStatusEnum} from '../enums/task-status-enum';
+import {generateUniqueIdWithTimestamp} from '../utils/generate-unique-id-with-timestamp';
 
 @Injectable({
   providedIn: 'root',
@@ -19,4 +22,20 @@ export class TaskService {
 
   private doneTasks$ = new BehaviorSubject<ITask[]>([]);
   readonly doneTasks = this.todoTasks$.asObservable();
+
+  addTask(taskInfos: ITaskFormControls) {
+    const newTask: ITask = {
+      ...taskInfos,
+      status: TaskStatusEnum.TODO,
+      id: generateUniqueIdWithTimestamp(),
+      comments: [],
+    };
+
+    const currentList = this.todoTasks$.value();
+
+    this.todoTasks$.next([
+      ...currentList,
+      newTask
+    ]);
+  }
 }
